3078887118dbf7aa35bdda7e9c546963d54bae16
anton-vm <anton_vm@ukr.net>
2020-02-03 22:26:44 +0300

    HW-07.2

--- JS-HW-module-07/js/task-06.js
+++ JS-HW-module-07/js/task-06.js
@@ -1,13 +1,20 @@
 const validation = document.querySelector("#validation-input")
+const length = document.querySelector("input")
 
-
+console.log(length.dataset);
 
 validation.onblur = function check () {
 
-    if (validation.value.length === 6) {
+    if (validation.value.length >= 6) {
         validation.classList.add('valid')
     } else {
         validation.classList.add('invalid')
     }
 }
 
+validation.onfocus = function  () {
+        validation.classList.remove('valid')
+        validation.classList.remove('invalid')
+        validation.innerHTML = ''
+  
+}
